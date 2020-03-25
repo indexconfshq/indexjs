dceaacae98b695f0afa2f88f800774d8ee345379
Luis Oliveira (xGeeks) <l.oliveira@xgeeks.io>
2020-03-25 15:30:40 +0000

    Add manifest settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  plugins: ["gatsby-plugin-theme-ui"],
+    plugins: [
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `indexjs`,
+                short_name: `indexjs`,
+                start_url: `/`,
+                background_color: `#07102b`,
+                theme_color: `#f8cd00`,
+                display: `standalone`,
+                icon: `src/images/logo.svg`,
+            },
+        },
+        'gatsby-plugin-theme-ui',
+    ],
 }
